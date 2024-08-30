f9f2d5e21327c3925f50a691c52c038de178d635
Umar Farooq <umarfarooq211203@gmail.com>
2024-08-30 22:11:05 +0500

    Adding few commnts in the code

--- CracksVisualization/cracksVisualize2.py
+++ CracksVisualization/cracksVisualize2.py
@@ -26,7 +26,6 @@ def visualize_potholes(images_dir, masks_dir):
         nFrame = frame_id + ".png"
         img = cv2.imread((os.path.join(images_dir, nFrame)))
         mask = cv2.imread((os.path.join(masks_dir, nFrame)))
-        mask2 = mask.copy()
         temp_img = img.copy()
         
         save = False
@@ -34,41 +33,22 @@ def visualize_potholes(images_dir, masks_dir):
             save = False
             print("Frame: ", frame_id, " Pothole Number:", pothole_data["pothole_num"])
             bbox = pothole_data["bbox"]
-            # score = pothole_data["Score"]
-            # detected = pothole_data["detected_count"]
             pothole_num = pothole_data["pothole_num"]
 
-            # if score >= 33 and detected > 1:
-            # if score >= 33:
             if True:
-                # score_list.append(score)
-                # score = "{:.2f}".format(score)
                 area  = pothole_data["area"]
                 save = True
                 print("Pothole Data:")
                 print("  Pothole Number:", pothole_data["pothole_num"])
                 print("  Bounding Box:", pothole_data["bbox"])
-                # print("  Score:", pothole_data["Score"])
-                # print("  Possible Count:", pothole_data["possible_count"])
-                # print("  Detected Count:", pothole_data["detected_count"])
                 print("  Detected Area:", pothole_data["area"])
-                # print("  Detected Area:", pothole_data["DetectionList"])
-                # scoreText = str(score)+"% "+str(detected)
-                # areaText = str(area)
-                # scoreText +=  "%_" + areaText
                 x, y, w, h = bbox
                 color = (0, 0, 255)  # Red color
                 thickness = 3
                 imgToSave = img.copy()
                 cv2.rectangle(imgToSave, (x, y), (x + w, y + h), color, thickness)
-                # cv2.putText(imgToSave, scoreText, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1)
                 cv2.rectangle(temp_img, (x, y), (x + w, y + h), color, thickness)
-                # cv2.putText(temp_img, scoreText, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0, 255), 1)
                 cv2.rectangle(mask, (x, y), (x + w, y + h), (255,0,0), thickness)
-                # cv2.putText(mask, scoreText, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 1)
-                cv2.rectangle(mask2, (x, y), (x + w, y + h), (255,0,0), -1)
-                # cv2.putText(mask2, scoreText, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 1)
-        # for bboxes in
                 if save:
                     print("Save TRUE")
                     savePath, filename = os.path.split(images_dir)
@@ -91,7 +71,6 @@ def visualize_potholes(images_dir, masks_dir):
             # print("Saving: ", save_img)
             cv2.imwrite(save_img + '/Images/' + frame_id + '_image.png', temp_img)
             cv2.imwrite(save_img + '/Masks/' + frame_id + '_mask.png', mask)
-            cv2.imwrite(save_img + '/Filled/' + frame_id + '_mask_filled.png', mask2)
             
             
             
--- main.py
+++ main.py
@@ -3,25 +3,9 @@ import os.path
 import csv
 import json
 import time
-from CracksProjection.init import init
 from CracksDictionaryCreation.trackCracks import track_cracks as track_cracks1
-# from CracksProjection.init import track_cracks as track_cracks2 
-from CracksProjection.trackCracks import track_cracks as track_cracks2 
+from CracksProjection.init import init
 from CracksVisualization.cracksVisualize2 import visualize_potholes
-
-def CracksDetection(image_dir,mask_dir,output_dir,old_json=""):
-    new_json = outputDir  
-    old_tracks = {}
-    tracked_potholes = {}
-    cropped_path = outputDir + "/cropped_images"
-    print(cropped_path)
-    if not os.path.exists(cropped_path):
-        os.mkdir(cropped_path)
-    print("Starting.................................")
-    tracked_potholes, old_tracks = track_cracks1(image_dir, mask_dir, old_json)
-    tracked_potholes = track_cracks2(image_dir, tracked_potholes, old_tracks, new_json, old_json, cropped_path)
-    visualize_potholes(image_dir, mask_dir)
-    print("Successful...............................")
 
 
 
@@ -54,38 +38,23 @@ if __name__ == '__main__':
     row1 = None
     row2 = None
     row3 = None
-    num_images = 65
+    num_images = 17
 
 
-    start_time = time.time()    
+    ### Start the Cracks Dictionary conataining every cracks either unique or duplication 
     tracked_potholes, old_tracks = track_cracks1(image_dir, mask_dir, old_json)
-    end_time = time.time()
-    row1 = ['A',str(num_images),str(int(end_time-start_time))]
     
-    # tracked_potholes = json.load(open("Testing/potholestracked.json"))
+    #### IF you Hva ealready stored the cracks in the a JSON File, you can just load by executing the 
+    #### line below and commenting the line above this comment calling the fucntion track_cracks1()
+    tracked_potholes = json.load(open(f"{outputDir}/potholestracked.json"))
     
-    start_time = time.time()
-    init(tracked_potholes,"Testing/")
-    end_time = time.time()
-    row2 = ['B',str(num_images),str(int(end_time-start_time))]
     
-    # start_time = time.time()
-    # tracked_potholes = track_cracks2(image_dir, tracked_potholes, old_tracks, new_json, old_json, cropped_path)
-    # end_time = time.time()
-    # row2 = ['B',str(num_images),str(int(end_time-start_time))]
+    ### Starts the Cracks Filtering 
+    init(tracked_potholes,outputDir + "/")
     
-    start_time = time.time()    
+    ### Start labelling the resulting unique cracks on the images and masks
     visualize_potholes(image_dir, mask_dir)
-    end_time = time.time()
-    row3 = ['C',str(num_images),str(int(end_time-start_time))]
 
-
-
-    with open('records.csv', mode='a', newline='') as file:
-        writer = csv.writer(file)
-        for i in [row1,row2,row3]:
-            if i is not None:
-                writer.writerow(i)
 
     print("Successful...............................")
     
